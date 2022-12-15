a6549c820aa1861c1f6cbf880b5dc97a9f502519
Sarmaon <patratiyaa@gmail.com>
2022-12-16 03:03:40 +0800

    switch Task03

---
+++ Task03/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задача №3
+Console.WriteLine("Введите первое число ");
+ int number1 = Convert.ToInt32(Console.ReadLine());
+
+ Console.WriteLine("Введите второе число ");
+ int number2 = Convert.ToInt32(Console.ReadLine());
+
+ int square = number2 * number2;
+
+     if (square == number1)
+ Console.WriteLine("Да");
+ else Console.WriteLine("Нет");
